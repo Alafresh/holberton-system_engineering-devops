96d88dabdef5b2ce28a3d4e82c0414cec9701f01
Alafresh <868@holbertonschool.com>
2019-12-09 17:54:37 +0000

    remove a parameter

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # this manifest kills a process named killmenow
 exec { 'killmenow':
-  command  => 'pkill -f killmenow',
-  provider => 'shell',
+  path    => '/usr/bin',
+  command => 'pkill -f killmenow',
 }
