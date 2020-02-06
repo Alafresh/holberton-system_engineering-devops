8d38757d5d5bd3278ec2f1247bf3ed33516979d3
Alafresh <868@holbertonschool.com>
2020-02-06 13:22:45 -0500

    pep8 fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,8 @@ import requests
 def number_of_subscribers(subreddit):
     """This function returns the number of subs"""
     subs = requests.get('https://www.reddit.com/r/{}/about.json'
-                        .format(subreddit), headers={'User-agent': 'AlafreshBot'})
+                        .format(subreddit),
+                        headers={'User-agent': 'AlafreshBot'})
     if subs.status_code == 404:
         return 0
     subs = subs.json()
