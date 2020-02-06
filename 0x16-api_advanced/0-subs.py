bd124dc1083199000928ae835bd81e2b06b8fd98
Alafresh <868@holbertonschool.com>
2020-02-06 12:46:06 -0500

    Upload readme

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+    subs = requests.get('https://www.reddit.com/r/{}/about.json'
+                        .format(subreddit), headers = {'User-agent': 'AlafreshBot'})
+    if subs.status_code == 404:
+        return 0
+    subs = subs.json()
+    for i in subs.get('data'):
+        if i == 'subscribers':
+            return subs.get('data')[i]
