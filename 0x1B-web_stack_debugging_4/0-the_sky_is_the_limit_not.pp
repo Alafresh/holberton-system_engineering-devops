dbdee89a8a01beb327562a5b7078c25596299c81
Alafresh <868@holbertonschool.com>
2020-02-27 14:30:38 -0500

    correction of path

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
 # requests served within a certain .
 exec {'adds max workers_connections':
-  command  => "sed -i 's/worker_processes 4;/worker_processes 100;/g' /etc/nginx/nginx.conf && sudo service nginx restart"
+  command  => "/bin/sed -i 's/worker_processes 4;/worker_processes 30;/g' /etc/nginx/nginx.conf | service nginx restart"
 }
